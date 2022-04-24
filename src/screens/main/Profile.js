cdd50c7c3db186b7b0ca6915843372bece1d4c06
Naol Dame <naododa5566@gmail.com>
2022-04-24 04:24:57 +0300

    Editing user profile

--- src/screens/main/Profile.js
+++ src/screens/main/Profile.js
@@ -12,6 +12,15 @@ const Profile = () => {
           <Text>Change Password</Text>
         </TouchableOpacity>
       </View>
+      <View style={styles.profilecontainer}>
+        <Image style={styles.headerImage} source={require("../../assets/svg/bottomNavbar/profile.svg")}
+                   
+        />
+      </View>
+
+      <TouchableOpacity style={}>
+
+      </TouchableOpacity>
 
     </SafeAreaView>
   );
@@ -37,6 +46,27 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     height: 20,
     width: 100,
+  },
+  profilecontainer: {
+    flexDirection: "row",
+    alignSelf: "center",
+  },
+  headerImage: {
+    justifyContent: "center",
+    backgroundColor: "red",
+    borderRadius: 100,
+    width: 150,
+    height: 150,
+  },
+  probtn: {
+    backgroundColor: "white",
+    borderColor: "black",
+    borderRadius: 50,
+    alignSelf: "center",
+    marginLeft: 90,
+    marginTop: -30,
+    // height:40,width:40
+    borderWidth: 1,
   },
 });
 export default Profile;
