2e43cde6ab117057756d533ce38d339b7e656dac
Naol Dame <naododa5566@gmail.com>
2022-04-24 04:15:37 +0300

    Creating Top Button change password button

--- src/screens/main/Profile.js
+++ src/screens/main/Profile.js
@@ -1,19 +1,42 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import React from 'react';
 
 import {CustomText} from '../../components/general/CustomText';
 
 const Profile = () => {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <CustomText content={'Profile'} />
-    </View>
+      <View style={styles.topbtn}>
+        <TouchableOpacity>
+          <Text>Change Password</Text>
+        </TouchableOpacity>
+      </View>
+
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    shadowColor: "black",
     flex: 1,
+    alignItems: "flex-start",
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 25,
+    zIndex: 10,
+  },
+  topbtn: {
+    margin: 5,
+    alignSelf: "flex-end",
+    alignItems: "center",
+    backgroundColor: "white",
+    borderColor: "black",
+    borderWidth: 0.5,
+    borderRadius: 50,
+    height: 20,
+    width: 100,
   },
 });
 export default Profile;
