2546fc653070e6b79da5a8e70fb8236b39939aa7
Naol Dame <naododa5566@gmail.com>
2022-05-13 13:32:44 +0300

    Creating user input

--- src/components/general/CustomInput Validation/CustomTextinputValidation.js
+++ src/components/general/CustomInput Validation/CustomTextinputValidation.js
@@ -5,3 +5,39 @@ import React from 'react';
 import Colors from '../../../constants/colors';
 import {isRequired} from 'react-native/Libraries/DeprecatedPropTypes/DeprecatedColorPropType';
 import {Controller} from 'react-hook-form';
+
+const CustomTextInputValidation = ({
+    label = null,
+    control = isRequired(),
+    rules = {required: true},
+    name = isRequired(),
+    customStyles = {},
+}) => {
+
+
+    return (
+        <View style={styles.container}>
+          {label && (
+            <CustomText
+              content={label}
+              fontColor={Colors.lightGray}
+              fontSize={16}
+            />
+          )}
+          <Controller
+            control={control}
+            rules={rules}
+            render={({field: {onChange, onBlur, value}}) => (
+              <TextInput
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+                style={[styles.textInput, customStyles]}
+              />
+            )}
+            name={name}
+          />
+        </View>
+    );
+};
+    
