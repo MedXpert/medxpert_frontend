f24103bce1eacf0297ecf549c2f24972f0780752
Naol Dame <naododa5566@gmail.com>
2022-05-13 14:02:20 +0300

    adding user and editing userform

--- src/screens/main/Profile.js
+++ src/screens/main/Profile.js
@@ -145,6 +145,31 @@ const style = StyleSheet.create({
     shadowColor: colors.gray,
     elevation: 3,
   },
-  
+  fullName: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignContent: 'center',
+    paddingTop: 10,
+  },
+  form: {
+    flexDirection: 'column',
+    paddingHorizontal: 30,
+    paddingTop: 20,
+  },
+});
 
-})
+const user = {
+  firstName: 'Naod',
+  lastName: 'Jordan',
+  email:'naod@gmail.com',
+  phone:'98479658428',
+  username:'naod',
+  sex: 'm',
+  address: 'some place, some city, some country',
+  profilePicture:
+    'https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80',
+
+};
+
+export default Profile;
+
