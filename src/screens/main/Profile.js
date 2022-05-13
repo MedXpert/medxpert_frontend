db1907c7f1b0a2242bfe54e51bfe3a2f37b8dfb0
Naol Dame <naododa5566@gmail.com>
2022-05-13 13:45:25 +0300

    Creating User Form

---
+++ src/screens/main/Profile.js
@@ -0,0 +1,21 @@
+import {View, Image, StyleSheet} from 'react-native';
+import React from 'react';
+import Colors from '../../constants/colors';
+import {CustomText} from '../../components/general/CustomText';
+import {CustomButton} from '../../components/general/CustomButton';
+import {CustomTextInputValidation} from '../../components/general/CustomTextInputValidation';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
+import IconAnt from 'react-native-vector-icons/AntDesign';
+import colors from '../../constants/colors';
+import {useForm} from 'react-hook-form';
+
+const Profile = ()=>{
+
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  }=
+
+
+}
