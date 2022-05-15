7133bc08cd3975542a3f9b0646f10f8e516e6a79
Naol Dame <naododa5566@gmail.com>
2022-05-16 00:59:36 +0300

    importing files

---
+++ src/components/general/CustomTextInputValidation/CustomTextInputValidation.js
@@ -0,0 +1,8 @@
+import {View, TextInput, StyleSheet} from 'react-native';
+import {CustomText} from '../CustomText/CustomText';
+import React from 'react';
+
+import Colors from '../../../constants/colors';
+import {isRequired} from 'react-native/Libraries/DeprecatedPropTypes/DeprecatedColorPropType';
+import {Controller} from 'react-hook-form';
+
