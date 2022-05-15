a0c334e7c9d7092f0c66ea427f65f039b83daeb2
Naol Dame <naododa5566@gmail.com>
2022-05-16 01:02:43 +0300

    update custom validation

--- src/components/general/CustomTextInputValidation/CustomTextInputValidation.js
+++ src/components/general/CustomTextInputValidation/CustomTextInputValidation.js
@@ -7,51 +7,50 @@ import {isRequired} from 'react-native/Libraries/DeprecatedPropTypes/DeprecatedC
 import {Controller} from 'react-hook-form';
 
 const CustomTextInputValidation = ({
-    label = null,
-    control = isRequired(),
-    rules = {required: true},
-    name = isRequired(),
-    customStyles = {},
-  }) => {
-    return (
-        <View style={styles.container}>
-          {label && (
-            <CustomText
-              content={label}
-              fontColor={Colors.lightGray}
-              fontSize={16}
-            />
-          )}
-          <Controller
-            control={control}
-            rules={rules}
-            render={({field: {onChange, onBlur, value}}) => (
-              <TextInput
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-                style={[styles.textInput, customStyles]}
-              />
-            )}
-            name={name}
+  label = null,
+  control = isRequired(),
+  rules = {required: true},
+  name = isRequired(),
+  customStyles = {},
+}) => {
+  return (
+    <View style={styles.container}>
+      {label && (
+        <CustomText
+          content={label}
+          fontColor={Colors.lightGray}
+          fontSize={16}
+        />
+      )}
+      <Controller
+        control={control}
+        rules={rules}
+        render={({field: {onChange, onBlur, value}}) => (
+          <TextInput
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            style={[styles.textInput, customStyles]}
           />
-        </View>
-      );
-    };
-    const styles = StyleSheet.create({
-        container: {marginBottom: 10},
-        textInput: {
-          alignItems: 'center',
-          justifyContent: 'space-between',
-          backgroundColor: Colors.white,
-          borderRadius: 10,
-          flexDirection: 'row',
-          height: 60,
-          color: Colors.gray,
-          fontSize: 18,
-          paddingHorizontal: 15,
-        },
-      });
-      
-      export {CustomTextInputValidation};
-            
+        )}
+        name={name}
+      />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  container: {marginBottom: 10},
+  textInput: {
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: Colors.white,
+    borderRadius: 10,
+    flexDirection: 'row',
+    height: 60,
+    color: Colors.gray,
+    fontSize: 18,
+    paddingHorizontal: 15,
+  },
+});
+
+export {CustomTextInputValidation};
