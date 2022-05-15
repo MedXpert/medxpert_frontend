895dd1b346b27e97135d27edf6bb7fe1d6344444
Naol Dame <naododa5566@gmail.com>
2022-05-16 01:00:22 +0300

    user input validation function

--- src/components/general/CustomTextInputValidation/CustomTextInputValidation.js
+++ src/components/general/CustomTextInputValidation/CustomTextInputValidation.js
@@ -6,3 +6,11 @@ import Colors from '../../../constants/colors';
 import {isRequired} from 'react-native/Libraries/DeprecatedPropTypes/DeprecatedColorPropType';
 import {Controller} from 'react-hook-form';
 
+const CustomTextInputValidation = ({
+    label = null,
+    control = isRequired(),
+    rules = {required: true},
+    name = isRequired(),
+    customStyles = {},
+  }) => {
+  
