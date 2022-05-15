7a730bad73f98fe242ba6f55b85114838f767d7b
Naol Dame <naododa5566@gmail.com>
2022-05-16 01:25:45 +0300

    adding controller for userform

--- src/screens/main/Profile.js
+++ src/screens/main/Profile.js
@@ -1,13 +1,36 @@
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Image, StyleSheet} from 'react-native';
 import React from 'react';
-
+import Colors from '../../constants/colors';
 import {CustomText} from '../../components/general/CustomText';
+import {CustomButton} from '../../components/general/CustomButton';
+import {CustomTextInputValidation} from '../../components/general/CustomTextInputValidation';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
+import IconAnt from 'react-native-vector-icons/AntDesign';
+import colors from '../../constants/colors';
+import {useForm} from 'react-hook-form';
 
 const Profile = () => {
+
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm({
+    defaultValues: {
+      fullName: user.firstName + ' ' + user.lastName,
+      email: user.email,
+      phone: user.phone,
+      username: user.username,
+      sex: user.sex,
+      address: user.address,
+    },
+  });
+  const onSubmit = data => console.log(data);
+
   return (
-    <View style={styles.container}>
-      <CustomText content={'Profile'} />
-    </View>
+    
+  
+    
   );
 };
 
