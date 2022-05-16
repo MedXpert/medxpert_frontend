cc3ed4b9cc039d2c9947b6c8f31cf66162956377
Naol Dame <naododa5566@gmail.com>
2022-05-16 15:36:16 +0300

    Create Emergency  Welcome page

---
+++ src/screens/main/Emergency/Welcome.js
@@ -0,0 +1,8 @@
+import {View, StyleSheet} from 'react-native';
+import React from 'react';
+import {CustomText} from '../../../components/general/CustomText';
+import {CustomButton} from '../../../components/general/CustomButton';
+import IconM from 'react-native-vector-icons/MaterialCommunityIcons';
+import colors from '../../../constants/colors';
+
+
