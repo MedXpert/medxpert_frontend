ce0a639ee9535e7efc879cedbf5f07b1290e6b2f
Naol Dame <naododa5566@gmail.com>
2022-05-16 15:37:36 +0300

    Creating Welcome page header

--- src/screens/main/Emergency/Welcome.js
+++ src/screens/main/Emergency/Welcome.js
@@ -5,4 +5,32 @@ import {CustomButton} from '../../../components/general/CustomButton';
 import IconM from 'react-native-vector-icons/MaterialCommunityIcons';
 import colors from '../../../constants/colors';
 
+export default function App() {
+    return (
+      <View style={styles.container}>
+        <View>
+          <CustomText
+            content="Click The turn on button to continue"
+            fontSize={17}
+            customStyles={styles.headerText}
+          />
+          <CustomText
+            content="Emergency alert for fall detection"
+            fontSize={40}
+            customStyles={styles.headerText2}
+          />
+        </View>
+        <View>
+          <View style={styles.icon1}>
+            <IconM name="hospital-building" color={colors.primary} size={170} />
+          </View>
+          <View style={styles.icon2}>
+            <IconM name="run" color={colors.primary} size={130} />
+          </View>
+          <View style={styles.icon3}>
+            <IconM name="ambulance" color="#049CE3" size={98} />
+          </View>
+        </View>
+        <View>
+  
 
