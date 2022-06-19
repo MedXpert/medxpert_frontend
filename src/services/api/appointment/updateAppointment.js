2f78608f1f39670d7ff4c5c48a2786e2f4d8cab1
Michael Belete <it.michael.belete@gmail.com>
2022-06-20 01:54:14 +0300

    fixed update

--- src/services/api/appointment/updateAppointment.js
+++ src/services/api/appointment/updateAppointment.js
@@ -2,7 +2,7 @@ import httpClient from "../../auth/httpClient";
 
 const updateAppointment = async appointment => {
   delete appointment.healthFacilityId;
-  return await httpClient.patch(`/appointments/${healthFacilityId}`, appointment, {
+  return await httpClient.put(`/appointments/${healthFacilityId}`, appointment, {
     headers: {
       'Content-Type': 'application/json',
     }
